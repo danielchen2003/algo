d523992652d67dbabff4d61a9721367521590a94
linfeng Chen <ku2003ku@gmail.com>
2022-09-20 14:41:44 -0400

    review

--- leetcode/092022/tree/medi/112.js
+++ leetcode/092022/tree/medi/112.js
@@ -15,3 +15,80 @@ let hasPathSum = function (root, targetsum) {
   if (!root) return false
   return traversal(root, targetsum - root.val)
 }
+
+
+
+class solution {
+  public boolean haspathsum(treenode root, int targetsum) {
+      if(root==null)return false;
+      stack<treenode> stack1 = new stack<>();
+      stack<integer> stack2 = new stack<>();
+      stack1.push(root);stack2.push(root.val);
+      while(!stack1.isempty()){
+          int size = stack1.size();
+          for(int i=0;i<size;i++){
+              treenode node = stack1.pop();int sum=stack2.pop();
+              // 如果该节点是叶子节点了，同时该节点的路径数值等于sum，那么就返回true
+              if(node.left==null && node.right==null && sum==targetsum)return true;
+              // 右节点，压进去一个节点的时候，将该节点的路径数值也记录下来
+              if(node.right!=null){
+                  stack1.push(node.right);stack2.push(sum+node.right.val);
+              }
+              // 左节点，压进去一个节点的时候，将该节点的路径数值也记录下来
+              if(node.left!=null){
+                  stack1.push(node.left);stack2.push(sum+node.left.val);
+              }
+          }
+      }
+      return false;
+  }   
+  
+}
+
+function hasPathSum(root,tar){
+  if(root==null) return false
+  let nodeStack =[root]
+  let valueStack = [root.val]
+  
+  while(nodeStack.length !== 0){
+      let size = stack.length
+    for(let i = 0; i < size; i++){
+    let curNode = nodeStack.pop()
+    let curValue = valueStack.pop()
+    if(!curNode.left && !curNode.right= && tar - curValue===0){
+      return true
+    }
+         if(curNode.right){
+      nodeStack.push(curNode.right)
+      valueStack.push(curValue + curNode.right.val)
+  }
+    if(curNode.left) {
+      nodeStack.push(curNode.left)
+      valueStack.push(curValue + curNode.left.val)
+    } 
+   
+    }
+}
+return false
+}
+
+
+var hasPathSum = function(root, sum) {
+	if(!root) return false;
+	const stack = [root];
+	const temps = [0];
+	let i = stack.length;
+	while(i) {
+		while(i--) {
+			const front = stack.pop();
+			let temp = temps.pop();
+			if(!front) continue;
+			temp += front.val;
+			if(!front.left && !front.right && temp === sum) return true;
+			stack.push(front.left, front.right);
+			temps.push(temp, temp);
+		}
+		i = stack.length;
+	}
+	return false;
+}
