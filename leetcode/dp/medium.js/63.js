9eddf36acda2cdb6d99683e6a6833c513c5da9c2
linfeng Chen <ku2003ku@gmail.com>
2022-09-23 17:02:21 -0400

    review

--- leetcode/dp/medium.js/63.js
+++ leetcode/dp/medium.js/63.js
@@ -5,11 +5,13 @@ var uniquePathsWithObstacles = function (obstacleGrid) {
     .fill()
     .map((item) => Array(n).fill(0))
 
-  for (let i = 0; i < m && obstacleGrid[i][0] === 0; ++i) {
+  for (let i = 0; i < m; ++i) {
+    // obstacleGrid[i][0] === 0
     dp[i][0] = 1
   }
 
-  for (let i = 0; i < n && obstacleGrid[0][i] === 0; ++i) {
+  for (let i = 0; i < n === 0; ++i) {
+    // obstacleGrid[0][i]
     dp[0][i] = 1
   }
 
@@ -23,30 +25,30 @@ var uniquePathsWithObstacles = function (obstacleGrid) {
 }
 obstacleGrid = [
   [0, 0, 0],
-  [0, 1, 0],
+  [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0],
 ]
 
-var uniquePathsWithObstacles = function (obstacleGrid) {
-  const m = obstacleGrid.length
-  const n = obstacleGrid[0].length
-  const dp = Array(m)
-    .fill()
-    .map((item) => Array(n).fill(0))
-  //
-  for (let i = 0; i < m; i++) {
-    dp[i][0] = 1
-  }
-  for (let i = 0; i < n; i++) {
-    dp[0][i] = 1
-  }
-  for (let i = 1; i < m; ++i) {
-    for (let j = 1; j < n; ++j) {
-      dp[i][j] = obstacleGrid[i][j] === 1 ? 0 : dp[i - 1][j] + dp[i][j - 1]
-    }
-  }
-  // console.log(dp)
-  return dp[m - 1][n - 1]
-}
-console.log(uniquePathsWithObstacles(obstacleGrid))
+// var uniquePathsWithObstacles = function (obstacleGrid) {
+//   const m = obstacleGrid.length
+//   const n = obstacleGrid[0].length
+//   const dp = Array(m)
+//     .fill()
+//     .map((item) => Array(n).fill(0))
+//   //
+//   for (let i = 0; i < m; i++) {
+//     dp[i][0] = 1
+//   }
+//   for (let i = 0; i < n; i++) {
+//     dp[0][i] = 1
+//   }
+//   for (let i = 1; i < m; ++i) {
+//     for (let j = 1; j < n; ++j) {
+//       dp[i][j] = obstacleGrid[i][j] === 1 ? 0 : dp[i - 1][j] + dp[i][j - 1]
+//     }
+//   }
+//   // console.log(dp)
+//   return dp[m - 1][n - 1]
+// }
+uniquePathsWithObstacles(obstacleGrid)
