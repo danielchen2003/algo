f361e875542a77969461817977f82f357017e227
linfeng Chen <ku2003ku@gmail.com>
2022-10-05 17:55:33 -0400

    review

--- leetcode/dp/medium.js/39.js
+++ leetcode/dp/medium.js/39.js
@@ -1,26 +1,42 @@
-var combinationSum = function(candidates, target) {
-  const ans = [];
+var combinationSum = function (candidates, target) {
+  const ans = []
   const dfs = (target, combine, idx) => {
-      if (idx === candidates.length) {
-          return;
-      }
-      if (target === 0) {
-          ans.push(combine);
-          return;
-      }
-      // 直接跳过
-      dfs(target, combine, idx + 1);
-      // 选择当前数
-      if (target - candidates[idx] >= 0) {
-          dfs(target - candidates[idx], [...combine, candidates[idx]], idx);
-      }
+    if (idx === candidates.length) {
+      return
+    }
+    if (target === 0) {
+      ans.push(combine)
+      return
+    }
+    // 直接跳过
+    dfs(target, combine, idx + 1)
+    // 选择当前数
+    if (target - candidates[idx] >= 0) {
+      dfs(target - candidates[idx], [...combine, candidates[idx]], idx)
+    }
   }
 
-  dfs(target, [], 0);
-  return ans;
-};
+  dfs(target, [], 0)
+  return ans
+}
 
-作者：LeetCode-Solution
-链接：https://leetcode.cn/problems/combination-sum/solution/zu-he-zong-he-by-leetcode-solution/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+var combinationSum = function (c, tar) {
+  let res = []
+  let len = c.length
+  function helper(tar, arr, idx) {
+    if (idx === len) {
+      return
+    }
+    if (tar < 0) return
+    if (tar === 0) {
+      res.push(arr)
+      return
+    }
+    helper(tar, arr, idx + 1)
+    if (tar - c[idx] >= 0) {
+      helper(tar - c[idx], [...arr, c[idx]], idx)
+    }
+  }
+  helper(tar, [], 0)
+  return res
+}
