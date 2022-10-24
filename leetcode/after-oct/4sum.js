b7fceeb26565557d251fc8930101600f6e11789f
linfeng Chen <ku2003ku@gmail.com>
2022-10-24 01:05:15 -0400

    review

--- leetcode/after-oct/4sum.js
+++ leetcode/after-oct/4sum.js
@@ -29,3 +29,42 @@ var fourSum = function (nums, target) {
   }
   return res
 }
+
+var fourSum = function (nums, target) {
+  let len = nums.length
+  if (len < 4) return []
+  nums.sort((a, b) => a - b)
+  if (nums[0] > target && target > 0) return []
+  let res = []
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue
+    for (let j = i + 1; j < nums.length; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue
+      let sum = nums[i] + nums[j]
+      if (sum > target && target > 0) {
+        break
+      }
+      let l = j + 1
+      let r = len - 1
+      while (l < r) {
+        let totalSum = nums[i] + nums[j] + nums[l] + nums[r]
+        if (totalSum === target) {
+          res.push(nums[i], nums[j], nums[l], nums[r])
+          while (nums[l] === nums[l + 1]) {
+            l++
+          }
+          while ((nums[r] = nums[r - 1])) {
+            r--
+          }
+          l++
+          r--
+        } else if (totalSum > target) {
+          r--
+        } else {
+          l++
+        }
+      }
+    }
+  }
+  return res
+}
